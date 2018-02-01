d6eca37c28e2b01032423fe39a1e64b32a3cdc9c
advanced_bear <advanced.bear@gmail.com>
2018-02-02 02:42:09 +0900

    change updater for installer version.

--- src/main.js
+++ src/main.js
@@ -1,34 +1,12 @@
 var gui = require('nw.gui');
-var https = require('https');
-var path = require('path');
-var fs = require('fs');
-var child_process = require('child_process');
 
-var url = 'https://dl.dropboxusercontent.com/s/dpw5qaj1kn16rm2/TwitchTalkApp.exe';
-var exePath = path.join(path.dirname(process.execPath),"TwitchTalkApp.exe");
-var cws = fs.createWriteStream(exePath);
+var url = 'https://dl.dropboxusercontent.com/s/ezcji8xo7u3jpyj/setup.exe';
 
 nw.Window.get().on('loaded', function(){
-    var req = https.get(url, function(res){
-        console.log(res.headers);
-        res.pipe(cws);
-        
-        res.on('end', function () {
-            console.log("download finish!");
-            $('.message').text("Launching new version...");
-            cws.end();
-            setTimeout(function(){
-                gui.Shell.openExternal(exePath);
-                setTimeout(function(){
-                    nw.Window.get().close();
-                },1000);
-            }, 2000);
-        }); 
-    })
-
-    req.on('error', function (err) {
-        alert('ダウンロードに失敗しました。手動でダウンロードしてください。');
-        gui.Shell.openExternal('https://advancedbear.github.io/products.html#TwitchTalkApp');
-        return;
-    });
+    setTimeout(function(){
+        gui.Shell.openExternal(url);
+        setTimeout(function(){
+            nw.Window.get().close();
+        },1000);
+    }, 2000);
 });
